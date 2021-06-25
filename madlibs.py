c4e13dbbd6f3f06231395fc8eb52cd116ce19eea
Arman Zaidi <zaidiarman90@gmail.com>
2021-06-25 22:50:20 +0530

    initail commit

---
+++ madlibs.py
@@ -0,0 +1,26 @@
+#This project is my first and is extremely basic one.
+#Many more to come!
+# THIS IS A POPULAR GAME CALLED MAD-LIBS
+
+adj1 = input('Adjective:  ')
+adj2 = input('Adjective:  ')
+type_of_bird = input('bird:  ')
+verb = input('Verb:  ')
+person_name = input('Name:  ')
+noun = input('Noun:  ')
+beverage = input('beverage:  ')
+body_part = input('body_part:  ')
+plural_noun = input('noun:  ')
+verb2 = input('verb:  ')
+noun2 = input('noun:  ')
+
+
+madlib = f"It was a {adj1} , cold November day. I woke up to the {adj2} smell of \
+{type_of_bird} roasting in the kitchen downstairs. I went down to see if i could \
+help {verb} the dinner. My mom said, 'See if {person_name} needs a fresh {noun} '.\
+So i carried a tray of glasses full of {beverage} into the living-room . When i \
+there i could not believe my {body_part} ! There were {plural_noun} {verb2} on the \
+{noun2} ."
+
+print(madlib)
+ 
